2f3ca2a64274d75a1187cfb736a60d45f4ceac66
guna sekhar <guna.hk93@gmail.com>
2019-06-02 16:58:08 +0530

    modifead code is

--- list_middle.py
+++ list_middle.py
@@ -1,28 +1,32 @@
-list = [67,67,767,345,7,2,34,45,4545,45]
-MiddleIndex=int(len(list)/2)
+my_input=input('enter the numbers that can store in the list  :')
+my_list=my_input.split(',')
+mylist=list(my_list)
+
+#mylist = [67,67,767,345,7,2,34,45,4545,45]
+MiddleIndex=int(len(mylist)/2)
 
 
-print('list is',list )
-print('Middle number in  list is ',list[MiddleIndex])
+print('mylist is',mylist )
+print('Middle item in list is ',mylist[MiddleIndex])
 
-print('starting list to middle is ',list[:MiddleIndex])
-print('Middlen list to end is ',list[MiddleIndex:])
+print('starting item in list to middle item in list is ',mylist[:MiddleIndex])
+print('Middle item in list to end item in list ',mylist[MiddleIndex:])
 
 
-Temp=list.copy()
-NewList=Temp[:MiddleIndex]
+Temp=mylist.copy()
+Newmylist=Temp[:MiddleIndex]
 
-list.sort()
-print('Sorted list in order',list)
+mylist.sort()
+print('Sorted mylist in order',mylist)
 
-print('sorting list in acending order to middle is ',list[:MiddleIndex])
-print('sorting list in acending order from middle to end is ',list[MiddleIndex:])
+print('sorted items in list in acending order to middle item is ',mylist[:MiddleIndex])
+print('sorted items in list in acending order from middle item to end item is ',mylist[MiddleIndex:])
 
-print('Smalest 3 numbers in list ',list[:3])
-print('Median number in list ',list[MiddleIndex+1])
+print('Smalest 3 items in list ',mylist[:3])
+print('Median number in list ',mylist[MiddleIndex+1])
 
-NewList.sort()
-print('starting list upto middle acending order is',NewList[:MiddleIndex])
+Newmylist.sort()
+print('starting item in mylist upto middle item in ascending order is',Newmylist[:MiddleIndex])
 
-NewList.sort(reverse=True)
-print('starting list in upto middle decending order is ',NewList[:MiddleIndex])
+Newmylist.sort(reverse=True)
+print('starting item in mylist  upto middle item in decending order is ',Newmylist[:MiddleIndex])
