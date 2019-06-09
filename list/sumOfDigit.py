ba2911bf2895022877ecbeac0e9bd698ee45caf7
guna sekhar <guna.hk444@gmail.com>
2019-06-09 10:31:44 +0530

    final modified list assianment

--- list/sumOfDigit.py
+++
@@ -1,6 +0,0 @@
-#calculate the sum of the digits in an integer
-
-input_Num = input('Enter the number  : ')
-inputNum = [int(i) for i in input_Num]
-print(f'The sum of digits of number is {sum(inputNum)}')
-
