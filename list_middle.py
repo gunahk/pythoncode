2d1bf0dc53be61f4accd3beda5e95a08da5bf395
guna sekhar <guna.hk444@gmail.com>
2019-06-29 00:16:00 +0530

    correction in listmiddle

--- list_middle.py
+++ list_middle.py
@@ -2,7 +2,6 @@ my_input=input('enter the numbers that can store in the list  :')
 my_list=my_input.split(',')
 mylist=list(my_list)
 
-#mylist = [67,67,767,345,7,2,34,45,4545,45]
 MiddleIndex=int(len(mylist)/2)
 
 
